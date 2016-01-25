28467ae9df40a55db8ffc99da70feada140f7b56
nanjiye123 <1902193503@qq.com>
2016-01-25 10:51:33 +0800

    add 02.c

---
+++ 03文件IO/02lseek.c
@@ -0,0 +1,30 @@
+//用于创建一个具有空洞的文件
+#include <apue.h>
+#include <fcntl.h>
+
+char buf1[] = "abcdefghij";
+char buf2[] = "ABCDEFGHIJ";
+
+int main()
+{
+	int fd;
+	if( (fd = creat("file.hole", FILE_MODE)) <0 )
+	{
+		err_sys("creat error");		
+	}
+	if( write(fd, buf1, 10) != 10 )//偏移为10
+	{
+		err_sys("buf1 write error");	
+	}
+	if( lseek(fd, 16384, SEEK_SET) == -1 )//偏移为16384
+	{
+		err_sys("lseek error");
+	}
+	if( write(fd, buf2, 10) != 10 )//偏移为16394
+	{
+		err_sys("buf2 write error");	
+	}
+	exit(0);
+}
+
+
