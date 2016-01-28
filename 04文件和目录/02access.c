a868c9bb73dd661454897d5b6ffc326dcc156e10
nanjiye123 <1902193503@qq.com>
2016-01-28 16:18:45 +0800

    add 02.c

---
+++ 04文件和目录/02access.c
@@ -0,0 +1,33 @@
+//进行访问权限测试
+#include <apue.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		err_quit("usage");	
+	}
+	if(access(argv[1], R_OK) < 0)//测试读权限
+	{
+		err_ret("access error for %s", argv[1]);	
+	}
+	else
+	{
+		printf("read access OK\n");	
+	}
+
+	if(open(argv[1], O_RDONLY) < 0)
+	{
+		err_ret("open error for %s", argv[1]);	
+	}
+	else
+	{
+		printf("open for reading OK\n");	
+	}
+	exit(0);
+}
+
+
+
+
